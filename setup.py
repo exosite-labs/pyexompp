4908ff60385325f79e42b35c01ab4f1b734c42b2
Kris Arnebeck <krisarnebeck@exosite.com>
2011-01-07 17:01:15 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (name='exompp',
+       version='0.1',
+       url = 'http://github.com/exosite-labs/exompp-python',
+       author = 'Exosite',
+       author_email = 'labs@exosite.com',
+       description = 'This client library supports the Exosite XMPP API.',
+       packages=['exompp'],
+       )
