b5774ec36377be77e4940ecc07b0b346afc8b4cd
hansr <labs@exosite.com>
2011-08-18 15:16:24 +0800

    cleaned up example app

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -17,20 +17,27 @@ from exompp.xmppchat import *
 #===============================================================================
 def main():
 #===============================================================================
-  ## setup our connection and client interface key values
-  connection = {'exosite_bot':'commander@m2.exosite.com','user_id':'hansandroid@m2.exosite.com','password':'spandr01d'}
-  cik = '9096951de12835f8c29212b08af95b05e42625de'
+  # Setup our connection and client interface key values
+  ## NOTE: the ENTERYOURXMPPACCOUNTNAMEHERE should be something like user@xmppserverdomain.com -> a google email account works just fine
+  ## NOTE: the ENTERYOURXMPPACCOUNTPASSWORDHERE should be the password associated with ENTERYOURXMPPACCOUNTNAMEHERE -> a google email account password works just fine
+  connection = {'exosite_bot':'commander@m2.exosite.com',
+                'user_id':'ENTERYOURXMPPACCOUNTNAMEHERE',
+                'password':'ENTERYOURXMPPACCOUNTPASSWORDHERE'}
+  
+  # Setup our Client Interface Key (CIK)
+  ## NOTE: PUTA40CHARACTERCIKHERE should be a valid Exosite Client Interface Key
+  ## The easiest way to get a CIK the first time is probably from Exosite Portals https://portals.exosite.com -> +Add Device
+  cik = 'PUTA40CHARACTERCIKHERE'
   
   publish = PublishToExosite(connection)
   publish.start()
   publish.addData(cik, "hello", "hello world!")  #client interface key, dataport alias, dataport value
   publish.stop()
   
-  print "\r\n\r\nSent \"Hello World!\" to your Portal!  View at one.exosite.com"
+  print "\r\n\r\nSent \"Hello World!\" to your Portal!  View at portals.exosite.com"
   
 
 
 #===============================================================================
 if __name__ == '__main__':
   sys.exit(main())
-
